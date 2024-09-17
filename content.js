cd9de7cf142cab970a50bd5b36d988091ad3e4f2
Jeff <jeff999955@gmail.com>
2024-09-17 12:19:55 -0700

    ChatGPT commit

---
+++ content.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    const targetElement = document.querySelector('#prompt-textarea');
+    if (targetElement) {
+        targetElement.focus();
+    }
+});
